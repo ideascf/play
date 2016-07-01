0387abe911aa39045e8c5736cf4d8886db14c4cb
chenfei <chenfei@qfpay.com>
2016-07-01 11:03:39 +0800

    Add. 增加thrift-RPC的简单测试，测试长连接RPC

---
+++ thrift_play/hello_svr/client.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from hello.Hello import Client
+
+def gen_client():
+    sock = TSocket.TSocket()
+    transport = TTransport.TBufferedTransport(sock)
+    proto = TBinaryProtocol.TBinaryProtocol(transport)
+
+    transport.open()
+    client = Client(proto)
+
+    return client
+
+def main():
+    client = gen_client()
+
+    print client.ping('ping')
+    print client.ping('ping')
+    print client.ping('ping')
+    print client.ping('ping')
+    print client.dozen()
+
+
+if __name__ == '__main__':
+    main()
