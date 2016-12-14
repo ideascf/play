27f11829a6ca0e82e4ff2aa8202e44ea388f2e42
chenfei <chenfei@qfpay.com>
2016-12-14 15:41:34 +0800

    update

--- gevent_test/switch_test.py
+++ gevent_test/switch_test.py
@@ -1,33 +1,78 @@
 # coding=utf-8
-import time
 from gevent import monkey
 monkey.patch_all()
 
+import GreenletProfiler
+import time
 import gevent
 
+
+
+GreenletProfiler.set_clock_type('cpu')
+
 cnt = 0
+alive = True
+
 
 def work():
-    global cnt
-    while True:
+    global cnt, alive
+
+    while alive:
         cnt += 1
         gevent.sleep()
 
         # print gevent.getcurrent()
 
-def main():
 
+def work2():
+    global cnt
+
+    cnt += 1
+    gevent.sleep()
+
+
+def start():
+    l = [
+        gevent.spawn(work)
+        for _ in range(1000)
+    ]
+    gevent.joinall(l)
+
+
+def start2():
+    global alive
+
+    from gevent import pool
+    p = pool.Pool(1000)
+
+    while alive:
+        p.spawn(work2)
+
+
+def main():
+    global alive
+
+    st = time.time()
     try:
-        l = [
-            gevent.spawn(work)
-            for _ in range(100)
-        ]
-        st = time.time()
-        gevent.joinall(l)
+        GreenletProfiler.start()
+        start()
     except KeyboardInterrupt:
+        alive = False
+    finally:
+        GreenletProfiler.stop()
+
         et = time.time()
-        print cnt
-        print et-st
+
+        print 'total time:', et - st
+        print 'CNT:', cnt
+        print 'QPS:', cnt / (et - st)
+        print
+        print
+
+        stats = GreenletProfiler.get_func_stats()
+        stats.print_all()
+        stats.save('profile.callgrind', type='callgrind')
+
 
 
 if __name__ == '__main__':
