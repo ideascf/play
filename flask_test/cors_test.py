57122c271c198acf87859fee4d3bec3accdf311a
chenfei <chenfei@qfpay.com>
2017-03-07 13:48:02 +0800

    增加cors_test.py例子

--- flask_test/cors_test.py
+++ flask_test/cors_test.py
@@ -5,7 +5,7 @@ from flask import Response
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', "HEAD", 'POST', 'OPTIONS', 'PUT', 'PATCH'])
+@app.route('/', methods=['GET', "HEAD", 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'])
 def main():
     print request.headers
     print request.method
@@ -26,14 +26,23 @@ def main():
     response.headers['Access-Control-Allow-Credentials'] = 'true'  # 必须是字符串的"true",不能是bool的True
 
     # 如果下面头部中指定的方法不包含当前请求的method，那么浏览器将不会交付给第三方网站
-    # 1. 该HEADER不针对GET、HEAD、POST、OPTIONS这4个方法，这几个方法始终可用
+    # 1. 该HEADER不针对GET、HEAD、POST、OPTIONS这4个方法，这几个方法如果满足"简单跨域请求"的要求，那么浏览器可以直接访问，不需要发出OPTIONS请求。
     # 2. PUT、DELETE、PATCH这三个方法，会先发出OPTIONS请求，然后根据OPTIONS请求中的Access-Control-Allow-Methods,
     #    来决定是否发出这个方法的请求.  如果当前METHOD不包含在内，浏览器不会发出请求。
     #     见: https://segmentfault.com/q/1010000005067552
     # 3. 区分大小写，存在多个时使用 ',' 分割
     # response.headers['Access-Control-Allow-Methods'] = request.method
-    # response.headers['Access-Control-Allow-Methods'] = ','.join(['GET', 'POST'])
+    response.headers['Access-Control-Allow-Methods'] = ','.join(['PATCH', 'PUT', 'DELETE'])
     # response.headers['Access-Control-Allow-Methods'] = 'PATCH'
+
+    # 当一个跨域请求发送简单跨域请求包括：请求方法为HEAD，GET，POST;
+    # 请求头只有4个字段，Accept，Accept-Language，Content-Language，Last-Event-ID;
+    # 如果设置了Content-Type，则其值只能是application/x-www-form-urlencoded,multipart/form-data,text/plain
+    # 例如： 如果请求头中带了，Content-Type,且不是"简单跨域请求"中定义的那几个。 那么浏览器会先发出OPTIONS请求，来确定是否允许带上Content-Type
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+
+    # 控制同类型请求的预检命令(OPTIONS)结果的缓存时间，用以提升性能，单位:秒
+    response.headers['Access-Control-Max-Age'] = '3600'
 
     return response
 
@@ -49,5 +58,11 @@ def set_cookie():
 
     return response
 
+@app.route('/ping', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+def ping():
+    response = Response()
+    response.set_data('pong')
+
+    return response
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
