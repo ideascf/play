41f958f37ed967d83a5c305c78d1730953fe236a
chenfei <chenfei@qfpay.com>
2017-11-17 16:19:30 +0800

    more

--- flask_test/ajax_array.py
+++ flask_test/ajax_array.py
@@ -4,11 +4,20 @@ from flask import request
 
 app = flask.Flask(__name__)
 
+@app.errorhandler(404)
+def error(*args, **kwargs):
+    print 'args', request.args
+    print 'form', request.form
+    print 'cookie', request.cookies
+
+    return 'hello'
 
 @app.route('/', methods=['get', 'post'])
 def index():
     with open('./index.html') as f:
         return f.read()
+
+
 
 @app.route('/p', methods=['get', 'post'])
 def p():
