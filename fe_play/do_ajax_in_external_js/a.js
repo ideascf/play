46e8726774f7d5e5c6a25205eedfb4d58b265462
chenfei <chenfei@qfpay.com>
2019-01-07 11:53:08 +0800

    commit some front-end playground code.

---
+++ fe_play/do_ajax_in_external_js/a.js
@@ -0,0 +1,13 @@
+$.ajax({
+    url: 'http://192.168.0.71:9999/b',
+    dataType: '"application/x-www-form-urlencoded"',
+    data: {
+
+    },
+    success: function(){
+        console.log('ok')
+    },
+    error: function(){
+
+    }
+ })
---
+++ fe_play/do_ajax_in_external_js/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python -m SimpleHTTPServer 9999 &
+python -m SimpleHTTPServer 8888
